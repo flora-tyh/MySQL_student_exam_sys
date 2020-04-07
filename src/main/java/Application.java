e640dac1e24c2916d43cdd4be9788f4cbde130f0
tyh <756467256@qq.com>
2020-04-07 08:54:01 +0800

    {YunHan}{fixed}--{MySQL}{student_exam_sys}:optimize the cycle

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,9 +5,8 @@ import Privilege.Teacher;
 public class Application {
 
   public static void main(String[] args) {
-    boolean isLoginSuccess = false;
     String privilege = null;
-    while (!isLoginSuccess) {
+    while (true) {
       boolean isLogout = false;
       showMainPage();
       privilege = login();
